b3b6e769e3fcdf3bbcc6be90374b4f07caf71b0d
mayurpatil1 <mayurpatil3555@gmail.com>
2024-01-07 08:40:39 -0800

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://mayur:mayur%40123@cluster0.9ljjkuw.mongodb.net/todos");
+mongoose.connect("mongodb+srv://xyz:xyz%40123@cluster0.9ljjkuw.mongodb.net/todos");
 
 const todoSchema = mongoose.Schema({
     title: String,
